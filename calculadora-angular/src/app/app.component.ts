0b066e6cafa9bca2b86575293bc0fdc440bda748
JoanR17 <joanr.m.1999@gmail.com>
2022-03-10 13:09:07 +0100

    Commit Calculadora

---
+++ calculadora-angular/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  operador = '0';
+  resultado = '0';
+
+  addNum(num: string)
+  {
+    if(this.resultado != '0')
+    {
+      this.operador = num;
+    }
+    else if(this.operador == '0' && num == '00')
+    {
+      this.operador = '0';
+    }
+    else if(this.operador == '0')
+    {
+      this.operador = num;
+    }
+    else
+    {
+      this.operador += num;
+    }
+    this.resultado = '0';
+  }
+
+  addOperador(op: string)
+  {
+    if(this.resultado != '0')
+    {
+      this.operador = this.resultado + op;
+    }
+    else
+    {
+      this.operador += op;
+    }
+    this.resultado = '0';
+  }
+
+  delete()
+  {
+    this.operador = this.operador.substring(0,this.operador.length-1);
+  }
+
+  evaluar()
+  {
+    this.resultado = eval(this.operador);
+  }
+}
